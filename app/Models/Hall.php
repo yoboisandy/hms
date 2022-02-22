a2ad1706c810c843c7788c225ee558904864f334
Asim Poudel <asim.pdl56@gmail.com>
2022-02-22 09:06:03 +0545

    a

--- app/Models/Hall.php
+++ app/Models/Hall.php
@@ -8,5 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Hall extends Model
 {
     use HasFactory;
+
     protected $fillable = ['name', 'description', 'base_occupancy', 'high_occupancy', 'amenity_id', 'floor_id', 'image', 'base_price', 'high_price'];
 }
