be89da264255f431c3e1d1948647b9b280562ecb
Asim Poudel <asim.pdl56@gmail.com>
2022-02-25 11:44:12 +0545

    a

--- database/seeders/FloorSeeder.php
+++ database/seeders/FloorSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Floor;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class FloorSeeder extends Seeder
 {
@@ -14,6 +15,34 @@ class FloorSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Floor::create([
+            "name" => "First",
+            "floor_number" => "1",
+            "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia exercitationem, nihil voluptatum quam iure esse! Nemo, sit pariatur a ea esse nam perferendis, vel obcaecati, reiciendis commodi voluptates. Temporibus, eum rerum. Provident reiciendis harum cumque velit nihil, optio soluta iusto?"
+        ]);
+        Floor::create([
+            "name" => "Second",
+            "floor_number" => "2",
+
+            "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia exercitationem, nihil voluptatum quam iure esse! Nemo, sit pariatur a ea esse nam perferendis, vel obcaecati, reiciendis commodi voluptates. Temporibus, eum rerum. Provident reiciendis harum cumque velit nihil, optio soluta iusto?"
+        ]);
+        Floor::create([
+            "name" => "Third",
+            "floor_number" => "3",
+
+            "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia exercitationem, nihil voluptatum quam iure esse! Nemo, sit pariatur a ea esse nam perferendis, vel obcaecati, reiciendis commodi voluptates. Temporibus, eum rerum. Provident reiciendis harum cumque velit nihil, optio soluta iusto?"
+        ]);
+        Floor::create([
+            "name" => "Fourth",
+            "floor_number" => "4",
+
+            "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia exercitationem, nihil voluptatum quam iure esse! Nemo, sit pariatur a ea esse nam perferendis, vel obcaecati, reiciendis commodi voluptates. Temporibus, eum rerum. Provident reiciendis harum cumque velit nihil, optio soluta iusto?"
+        ]);
+        Floor::create([
+            "name" => "Fifth",
+            "floor_number" => "5",
+
+            "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia exercitationem, nihil voluptatum quam iure esse! Nemo, sit pariatur a ea esse nam perferendis, vel obcaecati, reiciendis commodi voluptates. Temporibus, eum rerum. Provident reiciendis harum cumque velit nihil, optio soluta iusto?"
+        ]);
     }
 }
