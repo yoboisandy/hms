916d6d29721dab2ba9a1e645aa0cd358fa340cf9
Sandeep Sharma <1949sandeepsharma52@gmail.com>
2022-03-03 13:09:18 +0545

    roomtype seeder added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,8 @@ class DatabaseSeeder extends Seeder
             HousekeepingSeeder::class,
             EmployeeSeeder::class,
             CustomerSeeder::class,
+            FloorSeeder::class,
+            RoomtypeSeeder::class,
         ]);
     }
 }
