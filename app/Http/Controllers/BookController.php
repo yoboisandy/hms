84a0ab329df286bd7309be5823fa3eb16bf6f4bd
Asim Poudel <asim.pdl56@gmail.com>
2022-03-29 13:09:36 +0545

    Calculate bill from frontoffice and Kitchen model,migrations is created

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -5,12 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Book;
 use App\Models\Room;
 use App\Models\Roomtype;
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
-use phpDocumentor\Reflection\Types\Boolean;
-use Psr\Http\Message\ResponseInterface;
 
 class BookController extends Controller
 {
@@ -31,19 +28,25 @@ class BookController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, Roomtype $roomtype)
     {
         $request->validate([
-            'room_req' => ['required', 'gte:1'],
             'start_date' => ['bail', 'required',  'required_with:end_date', 'after_or_equal:' . now(),  'date', 'before_or_equal:end_date'],
             'end_date' => ['bail', 'required', 'required_with:start_date', 'date', 'after_or_equal:start_date'],
             'roomtype_id' => ['required'],
         ]);
         $start_date = Carbon::parse($request->start_date);
         $end_date = Carbon::parse($request->end_date);
-        $data = ['user_id' => Auth::user()->id, 'room_id' => $request->room_id, 'roomtype_id' => $request->roomtype_id, 'room_req' => $request->room_req,];
+        $data = ['user_id' => Auth::user()->id, 'room_id' => $request->room_id, 'roomtype_id' => $request->roomtype_id,];
         $data['start_date'] = $start_date;
         $data['end_date'] = $end_date;
+        $a = Roomtype::select('base_price')->where('id', '=', $data['roomtype_id'])
+            // ->pluck('base_price')
+            ->get('base_price');
+        $price = $a->sum('base_price');
+
+        $data['price'] = $price;
+
         //Counting room
         $room = Room::where('roomtype_id', '=', $data['roomtype_id'])->count();
         // return $room;
@@ -59,14 +62,12 @@ class BookController extends Controller
 
         $room_available = $room - $booking_rooms;
         // return $room_available;
-        if ($room_available < $data['room_req']) {
-            return response()->json(['message' => $room_available . ' room available!!!']);
-        }
+
         if ($room_available == 0) {
             return response()->json(['message' => 'no room available']);
         } else {
             Book::create($data);
-            $room_available = $room_available - $data['room_req'];
+            // $room_available = $room_available - $data['room_req'];
             return response()->json(['message' => 'Room booked Sucessfully']);
         }
     }
@@ -94,7 +95,7 @@ class BookController extends Controller
         $data = $request->validate([
             'room_id' => ['required', 'exists:rooms,id', 'unique:books,room_id',],
         ]);
-
+        $data['status'] = 'confirmed';
         $book->where('id', '=', $id)->update($data);
 
         return response()->json(['message' => 'Room allocated to that bookings!!']);
@@ -110,8 +111,9 @@ class BookController extends Controller
     {
         //
     }
-    public function calculate(Book $book, User $user, $id)
+    public function calculate($id)
     {
-        //
+        $room = Book::where('user_id', '=', $id)->sum('price');
+        return "Total price is: " . $room;
     }
 }
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -9,7 +9,7 @@ class Book extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['room_id', 'user_id', 'status', 'start_date', 'end_date', 'room_req', 'roomtype_id'];
+    protected $fillable = ['room_id', 'user_id', 'status', 'start_date', 'end_date', 'roomtype_id', 'status', 'price',];
 
     public function room()
     {
